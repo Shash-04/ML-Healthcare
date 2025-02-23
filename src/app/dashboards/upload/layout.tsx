e05a2dcac6ffc06fe2285a5054443f4f5580c13b
Shashwat Vaish <91148757+Shash-04@users.noreply.github.com>
2025-02-23 11:57:16 +0530

    Update layout.tsx

--- src/app/dashboards/upload/layout.tsx
+++ src/app/dashboards/upload/layout.tsx
@@ -8,12 +8,7 @@ export default function DashboardLayout({
 }) {
   return (
     
-    <div className="h-full relative">
-      <div className="z-50">
-      </div>
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
-        <Sidebar />
-      </div>
+    <div>
       <main className="md:pl-72">{children}</main>
     </div>
   )
